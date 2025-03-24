79d86a7cb857461c04f79ff80a6845d8c7a01de9
Stefan Koch <stefan.koch@ams-osram.com>
2025-03-24 16:12:34 +0100

    eigene qr codes

--- qr_code.py
+++ qr_code.py
@@ -2,32 +2,26 @@ import os
 import qrcode
 import base64
 
+data = {
+    'HomoSapiens': "https://skoc1974.github.io/steckbrief_homos/homo%20sapiens.html",
+    'HomoNeanderthalensis': "https://skoc1974.github.io/steckbrief_homos/homo%20neanderthalensis.html",
+    'HomoHeidelbergensis': "https://skoc1974.github.io/steckbrief_homos/homo%20heidelbergensis.html"
+}
 
-# Alle .html-Dateien im aktuellen Verzeichnis suchen
-html_files = [f for f in os.listdir() if f.endswith(".html")]
+for homo, url in data.items():
 
-for html_file in html_files:
-    with open(html_file, "r", encoding="utf-8") as file:
-        html_content = file.read()
-    
-    # HTML-Code in Base64 umwandeln
-    encoded_html = base64.b64encode(html_content.encode()).decode()
-    
-    # Data-URL erstellen
-    data_url = f"data:text/html;charset=utf-8;base64,{encoded_html}"
-    
-    # QR-Code generieren
-    qr = qrcode.make(data_url)
-    
-    # PNG-Datei speichern (gleicher Name wie HTML-Datei, aber mit .png)
-    qr_filename = os.path.splitext(html_file)[0] + ".png"
-    qr.save(qr_filename)
-    
-    print(f"QR-Code gespeichert: {qr_filename}")
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
 
-print("Fertig! Alle HTML-Dateien wurden in QR-Codes umgewandelt.")
+    qr.add_data(url)
+    qr.make(fit=True)
 
+    img = qr.make_image(fill='black', back_color='white')
 
-
+    img.save(f"{homo}.png")
 
 
